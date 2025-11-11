c9c331888f18b4e2327a8a17891ef6e4ea5d5f2e
Miautrax <rvadri.9.12@gmail.com>
2025-11-11 09:29:47 +0100

    Diablo

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -6,7 +6,52 @@
 /*   By: arivas-q <arivas-q@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:44:36 by arivas-q          #+#    #+#             */
-/*   Updated: 2025/10/30 13:44:37 by arivas-q         ###   ########.fr       */
+/*   Updated: 2025/11/11 09:24:09 by arivas-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "signals.h"
+#include "../minishell.h"
+
+/* Única variable global: indica si el shell está esperando un child */
+volatile sig_atomic_t in_child = 0;
+
+void	set_in_child(int val)
+{
+	if (val)
+		in_child = 1;
+	else
+		in_child = 0;
+}
+
+void	setup_signal_handlers(void)
+{
+	signal(SIGINT, on_sigint);
+	signal(SIGQUIT, on_sigquit);
+}
+
+void	execute_signals(t_sigstage stage, int ret)
+{
+	if (stage == SIGST_BEFORE_FORK)
+	{
+		set_in_child(1);
+	}
+	else if (stage == SIGST_IN_CHILD)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+	}
+	else if (stage == SIGST_AFTER_WAIT)
+	{
+		if (ret == 130)
+		{
+			write(1, "\n", 1);
+		}
+		else if (ret == 131)
+		{
+			write(1, "Quit (core dumped)\n", 20);
+		}
+		set_in_child(0);
+	}
+}
 
