c8f1a7774de9f0d1b285574c05ed1ea119a5dcd7
mruiz-ur <espartanmanu1122@gmail.com>
2026-01-12 13:05:34 +0100

    uwu update

--- src/utils_extra_extra.c
+++ src/utils_extra_extra.c
@@ -6,36 +6,23 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 12:36:49 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2026/01/12 12:49:31 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2026/01/12 13:03:03 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-static void	check_if_pipe(t_token *split, t_token *token, t_command *com, t_minishell *min)
-{
-	while (split && split->type != TOKEN_PIPE)
-		split = split->next;
-	if (!split)
-	{
-		move_tokens_to_command(token, com, min);
-		return ;
-	}
-}
-
-static void init_pipes_vars(t_command *com)
-{
-	com->next->in_fd = 0;
-	com->next->out_fd = 1;
-	com->next->redirs->fd = -1;
-	com->next->redirs->filename = NULL;
-	com->next->redirs->next = NULL;
-	com->next->redirs->type = 0;
-	com->next->argv = NULL;
-	com->next->redir_error = 0;
-	com->next->next = NULL;
-}
+// static void	check_if_pipe(t_token *split, t_token *token, t_command *com, t_minishell *min)
+// {
+// 	while (split && split->type != TOKEN_PIPE)
+// 		split = split->next;
+// 	if (!split)
+// 	{
+// 		move_tokens_to_command(token, com, min);
+// 		return ;
+// 	}
+//
 
 void	pipe_tokens_to_command(t_token *token, t_command *com, t_minishell *min)
 {
@@ -46,7 +33,13 @@ void	pipe_tokens_to_command(t_token *token, t_command *com, t_minishell *min)
 	next_split = NULL;
 	if (!token || !com || !min)
 		return ;
-	check_if_pipe(split, token, com, min);
+	while (split && split->type != TOKEN_PIPE)
+		split = split->next;
+	if (!split)
+	{
+		move_tokens_to_command(token, com, min);
+		return ;
+	}
 	next_split = split->next;
 	split->next = NULL;
 	if (next_split)
@@ -60,7 +53,8 @@ void	pipe_tokens_to_command(t_token *token, t_command *com, t_minishell *min)
 		if (!com->next)
 			return ;
 		ft_memset(com->next, 0, sizeof(t_command));
-		init_pipes_vars(com);
+		com->next->in_fd = 0;
+		com->next->out_fd = 1;
 	}
 	pipe_tokens_to_command(next_split, com->next, min);
 }
